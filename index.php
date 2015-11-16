fa3420fa802f210d5e1797169b5b88186dbf17c8
rgordonatrsgc <rgordon@rsgc.on.ca>
2015-11-16 15:00:27 +0000

    Very basic landing page with links to login and register pages.

---
+++ index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Site With Authentication Example</title>
+
+  <link rel="stylesheet" href="css/styles.css?v=1.0">
+
+  <!--[if lt IE 9]>
+  <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+  
+</head>
+
+<body>
+  <script src="js/scripts.js"></script>
+
+  <h1>Welcome</h1>
+  
+  <p><a href="login.php">Login</a></p>
+  
+  <p><a href="register.php">Register</a></p>
+
+</body>
+</html>
